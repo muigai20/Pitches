a022788b5a1ad46a2cd7d2b829d32d38ce626a2f
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-21 22:31:02 +0300

    registered the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,8 @@ def create_app(config_name):
     # Creating the app configurations.
     app.config.from_object(config_options[config_name])
 
-# Initializing flask extensions
-bootstrap.init_app(app)
+   # Initializing flask extensions
+   bootstrap.init_app(app)
+   # Registering the blueprint
+   from .main import main as main_blueprint
+   app.register_blueprint(main_blueprint)
